43f05323ae4bcf478b4f2100150d24419a84f3a9
barnettrp <barnettrp@gmail.com>
2025-09-07 20:56:24 +0000

    Update firebase-init.js

--- public/firebase-init.js
+++ public/firebase-init.js
@@ -30,7 +30,7 @@ const firebaseConfig = {
     apiKey: "AIzaSyDxVns7LxAG2WMkuUu8JOfgx7bE-6MycBY",
     authDomain: "spanish-ai-project.firebaseapp.com",
     projectId: "spanish-ai-project",
-    storageBucket: "spanish-ai-project.appspot.com",
+    storageBucket: "spanish-ai-project.appspot.com", // CORRECTED DOMAIN
     messagingSenderId: "788393465858",
     appId: "1:788393465858:web:ad0770073d59fa88a7af1b"
 };
