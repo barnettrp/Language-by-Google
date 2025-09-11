eb02b3bd98e18c7e36fe2d6cee4b7b89b066dfd1
barnettrp <barnettrp@gmail.com>
2025-09-11 11:43:03 -0400

    Update firebase-config.js

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -1,5 +1,6 @@
-// --- PASTE YOUR FIREBASE CONFIGURATION OBJECT ONCE AND FOR ALL ---
-// This file will hold your secret keys. The main index.html file will load this automatically.
+// --- PASTE YOUR FIREBASE CONFIGURATION OBJECT HERE ---
+// This file separates your Firebase keys from the main application logic.
+// You can find this object in your Firebase project settings.
 const firebaseConfig = {
   apiKey: "AIzaSyDxVns7LxAG2WMkuUu8JOfgx7bE-6MycBY",
   authDomain: "spanish-ai-project.firebaseapp.com",
@@ -8,6 +9,11 @@ const firebaseConfig = {
   messagingSenderId: "788393465858",
   appId: "1:788393465858:web:ad0770073d59fa88a7af1b"
 };
+
+// Make the config available to other scripts
+window.firebaseConfig = firebaseConfig;
+
+
 
 // Make the config available to other scripts
 window.firebaseConfig = firebaseConfig;
