583e0d767675c0b4f689cd3321b788814324b742
barnettrp <barnettrp@gmail.com>
2025-09-16 21:24:04 -0400

    Update firebase-init.js

--- public/firebase-init.js
+++ public/firebase-init.js
@@ -1,1 +1,8 @@
-
+const firebaseConfig = {
+  apiKey: "AIzaSyDxVns7LxAG2WMkuUu8JOfgx7bE-6MycBY",
+  authDomain: "spanish-ai-project.firebaseapp.com",
+  projectId: "spanish-ai-project",
+  storageBucket: "spanish-ai-project.firebasestorage.app",
+  messagingSenderId: "788393465858",
+  appId: "1:788393465858:web:ad0770073d59fa88a7af1b"
+};
